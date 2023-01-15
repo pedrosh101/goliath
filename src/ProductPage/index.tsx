6fe56b4ea858cfdf3d0fe78939caac8b0b61e989
pedrosh101 <pedro.sh101@gmail.com>
2023-01-14 23:25:52 -0300

    little css adjust

--- src/ProductPage/index.tsx
+++ src/ProductPage/index.tsx
@@ -8,9 +8,9 @@ const ProductPage = ({ title, description, image, tones }: IProduct) => {
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen bg-clr1 font-avenir p-6">
         <div className="flex flex-col md:pl-40 text-end justify-center">
-          <h1 className="text-6xl mb-5">{title}</h1>
+          <h1 className="text-5xl md:text-6xl mb-5">{title}</h1>
           <p className="text-2xl">{description}</p>
-          <p className="text-2xl italic mt-4 mb-2">Main Tones: {tones}</p>
+          <p className="text-1xl md:text-2xl italic mt-4 mb-2">Main Tones: {tones}</p>
           <Link href="/novo-produto">
             <h3 className="text-3xl font-bold text-red-200 mt-2 hover:text-red-300">Buy It</h3>
           </Link>
@@ -19,7 +19,7 @@ const ProductPage = ({ title, description, image, tones }: IProduct) => {
           <img
             src={image}
             alt="parfum"
-            className="py-16 md:object-left md:pl-28 md:p-12"
+            className="pt-16 md:object-left md:pl-28 md:p-12"
           ></img>
         </div>
       </div>
